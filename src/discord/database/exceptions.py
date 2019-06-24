c4fe438f2b525b6c00bfe7c1e4e7a98a8ce89ed8
ttgc <ttgc@users.noreply.github.com>
2019-06-24 21:21:25 +0200

    added exceptions

---
+++ src/discord/database/exceptions.py
@@ -0,0 +1,37 @@
+#!usr/bin/env python3.7
+#-*-coding:utf-8-*-
+
+import discord
+
+class DatabaseException(Exception):
+    pass
+
+class NoChannelFoundException(DatabaseException):
+    def __init__(self,channel: discord.TextChannel):
+        self.super()
+        self.channel = channel
+
+class NoEntryFoundException(DatabaseException):
+    def __init__(self,searching):
+        self.super()
+        self.entry = searching
+
+class TableDoesNotExist(DatabaseException):
+    def __init__(self,table):
+        self.super()
+        self.table = table
+
+class DatabaseDoesNotExist(DatabaseException):
+    def __init__(self,db):
+        self.super()
+        self.database = db
+
+class DatabaseAlreadyExist(DatabaseException):
+    def __init__(self,db):
+        self.super()
+        self.database = db
+
+class ServerNotFoundException(DatabaseException):
+    def __init__(self,srvid):
+        self.super()
+        self.serverID = srvid
