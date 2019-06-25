55c3abc22e1b3ab6f12eccd4fd87f0231b3c2122
ttgc <ttgc@users.noreply.github.com>
2019-06-25 14:05:15 +0200

    added event for guild available or not

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!usr/bin/env python3.7
 #-*-coding:utf-8-*-
 
+# import external and python libraries
 import discord
 from discord.ext import commands
 import asyncio
@@ -8,21 +9,58 @@ import logging
 import os
 import json
 
+# import personal libraries
 from src.logs import *
 
+# import cogs
 from src.discord.cogs.utils import *
 
+# =============== INIT ===============
+
+# logger init
 global logger
 logger = initlogs()
 
+# config init
+global config
+with open("config.json","r") as configfile:
+    config = json.load(config)
+# token init
 global TOKEN
-with open("config.json","r") as config:
-    TOKEN = json.load(config)["token"]
+TOKEN = config["token"]
 
+# client init
 global client
 client = discord.ext.commands.Bot('/',case_insensitive=True)
 
-# Code here
+# self-managed guild init
+global SELF_GUILD
+SELF_GUILD = None
+if config["self-guild"].get("mode","load") == "load":
+    SELF_GUILD = config["self-guild"]["ID"]
+
+# =============== CORE ===============
+
+# events
+@client.event
+async def on_guild_available(guild):
+    global SELF_GUILD, logger, client
+    if guild == SELF_GUILD:
+        logger.warning("The self managed guild has become avalaible again, reconnecting, and enabling bot")
+        for cogname, cog in client.cogs:
+            if cogname != "Utils":
+                for cmd in cog.get_commands():
+                    cmd.update(enabled=True)
+
+@client.event
+async def on_guild_unavailable(guild):
+    global SELF_GUILD, logger, client
+    if guild == SELF_GUILD:
+        logger.warning("The self managed guild has become unavalaible, waiting for reconnect, and disabling bot")
+        for cogname, cog in client.cogs:
+            if cogname != "Utils":
+                for cmd in cog.get_commands():
+                    cmd.update(enabled=False)
 
 @client.event
 async def on_ready():
@@ -35,12 +73,14 @@ async def on_ready():
     logger.info("Generate invite link : %s",url)
     logger.info("Bot is now ready")
 
+# =============== MAIN ===============
 async def main():
     global TOKEN
     client.add_cog(Utils(client,logger))
     await client.login(TOKEN)
     await client.connect()
 
+# ================ RUN ================
 loop = asyncio.get_event_loop()
 try:
     loop.run_until_complete(main())
