643e7ba082cc670666e63735d5abba8705b2885f
ttgc <ttgc@users.noreply.github.com>
2019-06-28 14:47:35 +0200

    update wallet command to fit new role system

--- src/discord/cogs/economy.py
+++ src/discord/cogs/economy.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 from src.discord.checks import *
 from src.discord.database.database import *
 from src.discord.database.table import *
+from src.discord.manage import *
 import logging
 
 class Economy(commands.Cog):
@@ -16,6 +17,7 @@ class Economy(commands.Cog):
     @commands.check(check_inserv)
     @commands.command(aliases=["money", "coins"])
     async def wallet(self,ctx):
+        gold, plat, dark = await manage_roles(ctx, self.logger)
         db = Database(self.bot,self.logger,ctx.guild.id,"selfguild")
         usertable = Table(db,"user")
         userlist = await usertable.fetch()
@@ -23,20 +25,20 @@ class Economy(commands.Cog):
             if str(ctx.author.id) == user[0]: break
 
         money = int(user[1])
-        if money <= 1000:
-            color = int("FF0000",16)
-            card = "https://vignette.wikia.nocookie.net/soulcontrol/images/f/f9/Kimi_card.jpg/revision/latest?cb=20110508191032"
-        elif money <= 100000:
-            color = int("FFFF00",16)
-            card = "https://vignette.wikia.nocookie.net/soulcontrol/images/e/ee/Jenny_card.jpg/revision/latest?cb=20110508191130"
-        elif money <= 10000000:
-            color = int('3CD070',16)
-            card = "https://vignette.wikia.nocookie.net/soulcontrol/images/d/d5/Kou_card.jpg/revision/latest?cb=20110508191202"
-        else:
-            color = int('010101',16)
+        if dark in ctx.author.roles:
+            color = dark.color
             card = "https://vignette.wikia.nocookie.net/soulcontrol/images/5/50/Mikuni_card.jpg/revision/latest?cb=20110508191230"
+        elif plat in ctx.author.roles:
+            color = plat.color
+            card = "https://vignette.wikia.nocookie.net/soulcontrol/images/d/d5/Kou_card.jpg/revision/latest?cb=20110508191202"
+        elif gold in ctx.author.roles:
+            color = gold.color
+            card = "https://vignette.wikia.nocookie.net/soulcontrol/images/e/ee/Jenny_card.jpg/revision/latest?cb=20110508191130"
+        else:
+            color = discord.Color(int("FF0000",16))
+            card = "https://vignette.wikia.nocookie.net/soulcontrol/images/f/f9/Kimi_card.jpg/revision/latest?cb=20110508191032"
 
-        embd = discord.Embed(title="Current wallet",description="Financial district bank account information",colour=discord.Color(color))
+        embd = discord.Embed(title="Current wallet",description="Financial district bank account information",colour=color)
         embd.set_footer(text="Bot created by Ttgc and Trakozz",icon_url=self.bot.user.avatar_url)
         embd.set_thumbnail(url=card)
         embd.set_author(name=str(ctx.author),icon_url=ctx.author.avatar_url)
